fdb00c6c300071056cc2c2371518faaae6d7169d
Tomer <tomer.sedan@protonmail.com>
2020-09-11 17:48:37 -0700

    basic iqapi calls

--- commands/air.js
+++ commands/air.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const { airvisual } = require('../config.json');
+const https = require('https');
 
 module.exports = {
 	name: 'air',
@@ -7,10 +9,42 @@ module.exports = {
     aliases: ['weather'],
     args: false,
 	execute(message, args) {
-        const airEmbed = new Discord.MessageEmbed()
-        	.setColor('#eeeeee')
-        	.setTitle('the air is not good')
+		https.get(`https://api.airvisual.com/v2/nearest_city?key=${airvisual}`, (res) => {
+			let data = '';
+			res.on('data', (chunk) => { data += chunk });
 
-		message.channel.send(airEmbed);
+			res.on('end', () => {
+				let parsed = JSON.parse(data);
+				if (parsed.status != "success") return message.channel.send("the IQair api is kinda scuffed atm, sorry");
+				parsed = parsed.data;
+
+		    	console.log(`curled IQair for ${parsed.city}`);
+
+				const dp = Math.round(parsed.current.weather.tp - ((100-parsed.current.weather.hu)/5));
+
+				const desc = {
+					temperature: parsed.current.weather.tp + `°C or ${Math.round(parsed.current.weather.tp*1.8+32)}°F`,
+					humidity: parsed.current.weather.hu + '%',
+					dew: dp + '°C | ' + (dp <= 13 ? 'dry' : (dp <= 19 ? 'humid' : 'muggy')),
+				}
+
+				const airEmbed = new Discord.MessageEmbed()
+		        	.setColor('#f0932b')
+		        	.setTitle(parsed.current.pollution.aqius + ', mainly ' + parsed.current.pollution.mainus)
+					.setDescription(`Air quality for the city of ${parsed.city}`)
+					.addFields(
+						{name: 'Temperature', value: desc.temperature, inline: true},
+						{name: 'Humidity', value: desc.humidity, inline: true},
+						{name: 'Dew Point', value: desc.dew, inline: true},
+					)
+					.addField('Advisory','coming soon')
+					.setTimestamp()
+					.setFooter('blame IQair api not apocalypsebot pls uwu')
+
+				message.channel.send(airEmbed);
+			});
+		}).on("error", (err) => {
+			console.log(err.message);
+		});
 	},
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, discord } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -59,4 +59,4 @@ client.on('message', message => {
     }
 });
 
-client.login(token);
+client.login(discord);
